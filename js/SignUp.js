9e6fb33f0a3638666540d43871f6ed97fcb33cd3
unknown <dasarnab389@gmail.com>
2023-10-15 00:17:26 +0530

    complete

--- js/SignUp.js
+++ js/SignUp.js
@@ -6,6 +6,15 @@ document.getElementById('closeOverlay').addEventListener('click', function() {
     document.getElementById('signupOverlay').style.display = 'none';
 });
 
+document.getElementById('signupButtonMd').addEventListener('click', function() {
+    document.getElementById('signupOverlayMd').style.display = 'block';
+});
+
+document.getElementById('closeOverlayMd').addEventListener('click', function() {
+    document.getElementById('signupOverlayMd').style.display = 'none';
+});
+
+
 function flip(){
     document.getElementById("form").style.
         transform='rotateY(180deg)';
@@ -14,13 +23,23 @@ function flip1(){
     document.getElementById("form").style.
         transform='rotateY(0deg)';
 }
-
-function flip(){
-    document.getElementById("form").style.
-    transform='rotateY(180deg)';
+function flip2(){
+    document.getElementById("formMd").style.
+        transform='rotateY(180deg)';
+}
+function flip3(){
+    document.getElementById("formMd").style.
+        transform='rotateY(0deg)';
 }
 
+
 document.getElementById('submit').addEventListener('click', function() {
+    document.getElementById('signupOverlay').style.display = 'none';
+    document.getElementById('account').style.opacity = 0;
+    document.getElementById('accountCreated').style.display = 'flex';
+});
+
+document.getElementById('create').addEventListener('click', function() {
     document.getElementById('signupOverlay').style.display = 'none';
     document.getElementById('account').style.opacity = 0;
     document.getElementById('accountCreated').style.display = 'flex';
