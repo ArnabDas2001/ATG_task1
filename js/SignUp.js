87d5de4835f74bb58e9a679878734c70c832c887
unknown <dasarnab389@gmail.com>
2023-10-14 12:05:23 +0530

    sign up complete

--- js/SignUp.js
+++ js/SignUp.js
@@ -5,3 +5,18 @@ document.getElementById('signupButton').addEventListener('click', function() {
 document.getElementById('closeOverlay').addEventListener('click', function() {
     document.getElementById('signupOverlay').style.display = 'none';
 });
+
+function flip(){
+    document.getElementById("form").style.
+        transform='rotateY(180deg)';
+}
+function flip1(){
+    document.getElementById("form").style.
+        transform='rotateY(0deg)';
+}
+
+
+function flip(){
+    document.getElementById("form").style.
+    transform='rotateY(180deg)';
+}
